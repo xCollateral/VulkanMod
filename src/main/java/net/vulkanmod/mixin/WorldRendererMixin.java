2b77d51029a0329a8952b25334d8580a33673dfb
tr7zw <tr7zw@live.de>
2022-06-16 22:28:51 +0200

    Remove unused imports

--- src/main/java/net/vulkanmod/mixin/WorldRendererMixin.java
+++ src/main/java/net/vulkanmod/mixin/WorldRendererMixin.java
@@ -27,9 +27,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
