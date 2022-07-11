8c76f657d01aee3a06e6a9cc237eb24c15e6aed3
Collateral <>
2022-07-11 18:09:39 +0200

    get mod version

--- src/main/java/net/vulkanmod/Initializer.java
+++ src/main/java/net/vulkanmod/Initializer.java
@@ -7,6 +7,8 @@ import net.fabricmc.fabric.api.renderer.v1.material.MaterialFinder;
 import net.fabricmc.fabric.api.renderer.v1.material.RenderMaterial;
 import net.fabricmc.fabric.api.renderer.v1.mesh.MeshBuilder;
 import net.fabricmc.fabric.impl.renderer.RendererAccessImpl;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,10 +21,22 @@ import static org.objectweb.asm.Opcodes.*;
 public class Initializer implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger("VulkanMod");
 
+	private static String VERSION;
+
 	@Override
 	public void onInitialize() {
 
+		VERSION = FabricLoader.getInstance()
+				.getModContainer("vulkanmod")
+				.get()
+				.getMetadata()
+				.getVersion().getFriendlyString();
+
 		LOGGER.info("Hello Fabric world!");
 
+	}
+
+	public static String getVersion() {
+		return VERSION;
 	}
 }
