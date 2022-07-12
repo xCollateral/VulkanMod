5b374af41e7e140df5cf14f08ec1ec23e1485e12
Vcmp <N0755546@my.ntu.ac.uk>
2022-07-12 21:33:33 +0100

    Allow Vulkan Version to be displayed on the Debug GUI

--- src/main/java/net/vulkanmod/vulkan/DeviceInfo.java
+++ src/main/java/net/vulkanmod/vulkan/DeviceInfo.java
@@ -6,12 +6,15 @@ import oshi.hardware.CentralProcessor;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
+import static org.lwjgl.vulkan.VK10.*;
+
 public class DeviceInfo {
 
     public static final String cpuInfo;
     public static final String vendorId;
     public static final String deviceName;
     public static final String driverVersion;
+    public static String VkVersion;
 
     static {
         CentralProcessor centralProcessor = new SystemInfo().getHardware().getProcessor();
@@ -31,6 +34,8 @@ public class DeviceInfo {
 //        deviceName = new String(bytes, StandardCharsets.UTF_8);
             deviceName = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(bytes)).toString();
             driverVersion = String.valueOf(Vulkan.deviceProperties.driverVersion());
+            int va=Vulkan.deviceProperties.apiVersion();
+            VkVersion = String.valueOf((VK_VERSION_MAJOR(va) +"."+ VK_VERSION_MINOR(va) +"."+ VK_VERSION_PATCH(va)));
         }
         else {
             vendorId = "n/a";
