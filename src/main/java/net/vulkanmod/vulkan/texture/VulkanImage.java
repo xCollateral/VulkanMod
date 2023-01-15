c4fe779f33e1489fe9dedf4439169840547c4cd4
Vcmp <43959737+Vcmp@users.noreply.github.com>
2023-01-15 16:46:37 +0000

    fix stage barrier

--- src/main/java/net/vulkanmod/vulkan/texture/VulkanImage.java
+++ src/main/java/net/vulkanmod/vulkan/texture/VulkanImage.java
@@ -570,8 +570,8 @@ public class VulkanImage {
                 case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL + VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL -> {
                     barrier.srcAccessMask(VK_ACCESS_TRANSFER_WRITE_BIT);
                     barrier.dstAccessMask(VK_ACCESS_TRANSFER_READ_BIT);
-                    sourceStage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
-                    destinationStage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
+                    sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT ;
+                    destinationStage = VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT;
                 }
                 case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL + VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL -> {
                     barrier.srcAccessMask(VK_ACCESS_TRANSFER_WRITE_BIT);
