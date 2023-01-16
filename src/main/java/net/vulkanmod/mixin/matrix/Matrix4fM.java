23eeb00ea7a5e73dd38204fe8ee2213d3f9bf5a9
Collateral <>
2023-01-16 11:03:24 +0100

    compatibility

---
+++ src/main/java/net/vulkanmod/mixin/matrix/Matrix4fM.java
@@ -0,0 +1,27 @@
+package net.vulkanmod.mixin.matrix;
+
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(Matrix4f.class)
+public class Matrix4fM {
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite(remap = false)
+    public Matrix4f setOrtho(float left, float right, float bottom, float top, float zNear, float zFar) {
+        return new Matrix4f().setOrtho(left, right, bottom, top, zNear, zFar, true);
+    }
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite(remap = false)
+    public Matrix4f ortho(float left, float right, float bottom, float top, float zNear, float zFar) {
+        return new Matrix4f().ortho(left, right, bottom, top, zNear, zFar, true);
+    }
+}
