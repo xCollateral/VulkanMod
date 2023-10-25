fd6becc23082a79a8a64533f6fec009e80e56703
xCollateral <>
2023-10-25 23:05:22 +0200

    Added glsl includer

--- src/main/java/net/vulkanmod/vulkan/shader/SPIRVUtils.java
+++ src/main/java/net/vulkanmod/vulkan/shader/SPIRVUtils.java
@@ -1,24 +1,80 @@
 package net.vulkanmod.vulkan.shader;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
 import org.lwjgl.system.NativeResource;
+import org.lwjgl.util.shaderc.ShadercIncludeResolveI;
+import org.lwjgl.util.shaderc.ShadercIncludeResult;
+import org.lwjgl.util.shaderc.ShadercIncludeResultReleaseI;
+import org.lwjgl.vulkan.VK12;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.lwjgl.system.MemoryUtil.NULL;
+import static org.lwjgl.system.MemoryUtil.memASCII;
 import static org.lwjgl.util.shaderc.Shaderc.*;
 
 public class SPIRVUtils {
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
     private static final boolean OPTIMIZATIONS = false;
 
     private static long compiler;
+    private static long options;
+
+    //The dedicated Includer and Releaser Inner Classes used to Initialise #include Support for ShaderC
+    private static final ShaderIncluder SHADER_INCLUDER = new ShaderIncluder();
+    private static final ShaderReleaser SHADER_RELEASER = new ShaderReleaser();
+    private static final long pUserData = 0;
+
+    private static ObjectArrayList<String> includePaths;
+
+    private static float time = 0.0f;
+
+    static {
+        initCompiler();
+    }
+
+    private static void initCompiler() {
+        compiler = shaderc_compiler_initialize();
+
+        if(compiler == NULL) {
+            throw new RuntimeException("Failed to create shader compiler");
+        }
+
+        options = shaderc_compile_options_initialize();
+
+        if(options == NULL) {
+            throw new RuntimeException("Failed to create compiler options");
+        }
+
+        if(OPTIMIZATIONS)
+            shaderc_compile_options_set_optimization_level(options, shaderc_optimization_level_performance);
+
+        if(DEBUG)
+            shaderc_compile_options_set_generate_debug_info(options);
+
+        shaderc_compile_options_set_target_env(options, shaderc_env_version_vulkan_1_2, VK12.VK_API_VERSION_1_2);
+        shaderc_compile_options_set_include_callbacks(options, SHADER_INCLUDER, SHADER_RELEASER, pUserData);
+
+        includePaths = new ObjectArrayList<>();
+        addIncludePath("/assets/vulkanmod/shaders/include/");
+    }
+
+    public static void addIncludePath(String path) {
+        URL url = SPIRVUtils.class.getResource(path);
+
+        if(url != null)
+            includePaths.add(url.toExternalForm());
+    }
 
     public static SPIRV compileShaderAbsoluteFile(String shaderFile, ShaderKind shaderKind) {
         try {
@@ -31,24 +87,7 @@ public class SPIRVUtils {
     }
 
     public static SPIRV compileShader(String filename, String source, ShaderKind shaderKind) {
-
-        if(compiler == 0) compiler = shaderc_compiler_initialize();
-
-        if(compiler == NULL) {
-            throw new RuntimeException("Failed to create shader compiler");
-        }
-
-        long options = shaderc_compile_options_initialize();
-
-        if(options == NULL) {
-            throw new RuntimeException("Failed to create compiler options");
-        }
-
-        if(OPTIMIZATIONS)
-            shaderc_compile_options_set_optimization_level(options, shaderc_optimization_level_performance);
-
-        if(DEBUG)
-            shaderc_compile_options_set_generate_debug_info(options);
+        long startTime = System.nanoTime();
 
         long result = shaderc_compile_into_spv(compiler, source, shaderKind.kind, filename, "main", options);
 
@@ -59,6 +98,8 @@ public class SPIRVUtils {
         if(shaderc_result_get_compilation_status(result) != shaderc_compilation_status_success) {
             throw new RuntimeException("Failed to compile shader " + filename + " into SPIR-V:\n" + shaderc_result_get_error_message(result));
         }
+
+        time += (System.nanoTime() - startTime) / 1000000.0f;
 
         return new SPIRV(result, shaderc_result_get_bytes(result));
     }
@@ -87,6 +128,48 @@ public class SPIRVUtils {
 
         ShaderKind(int kind) {
             this.kind = kind;
+        }
+    }
+
+    private static class ShaderIncluder implements ShadercIncludeResolveI {
+
+        private static final int MAX_PATH_LENGTH = 4096; //Maximum Linux/Unix Path Length
+
+        @Override
+        public long invoke(long user_data, long requested_source, int type, long requesting_source, long include_depth) {
+            var requesting = memASCII(requesting_source);
+            var requested = memASCII(requested_source);
+
+            try(MemoryStack stack = MemoryStack.stackPush()) {
+                Path path;
+
+                for(String includePath : includePaths) {
+                    path = Paths.get(new URI(String.format("%s%s", includePath, requested)));
+
+                    if(Files.exists(path)) {
+                        byte[] bytes = Files.readAllBytes(path);
+
+                        return ShadercIncludeResult.malloc(stack)
+                                .source_name(stack.ASCII(requested))
+                                .content(stack.bytes(bytes))
+                                .user_data(user_data).address();
+                    }
+                }
+            } catch (IOException | URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+
+            throw new RuntimeException(String.format("%s: Unable to find %s in include paths", requesting, requested));
+        }
+    }
+
+    //TODO: Don't actually need the Releaser at all, (MemoryStack frees this for us)
+    //But ShaderC won't let us create the Includer without a corresponding Releaser, (so we need it anyway)
+    private static class ShaderReleaser implements ShadercIncludeResultReleaseI {
+
+        @Override
+        public void invoke(long user_data, long include_result) {
+            //TODO:Maybe dump Shader Compiled Binaries here to a .Misc Diretcory to allow easy caching.recompilation...
         }
     }
 
---
+++ src/main/resources/assets/vulkanmod/shaders/include/light.glsl
@@ -0,0 +1,29 @@
+//light.glsl
+#define MINECRAFT_LIGHT_POWER   (0.6)
+#define MINECRAFT_AMBIENT_LIGHT (0.4)
+
+vec4 minecraft_mix_light(vec3 lightDir0, vec3 lightDir1, vec3 normal, vec4 color) {
+    lightDir0 = normalize(lightDir0);
+    lightDir1 = normalize(lightDir1);
+    float light0 = max(0.0, dot(lightDir0, normal));
+    float light1 = max(0.0, dot(lightDir1, normal));
+    float lightAccum = min(1.0, (light0 + light1) * MINECRAFT_LIGHT_POWER + MINECRAFT_AMBIENT_LIGHT);
+    return vec4(color.rgb * lightAccum, color.a);
+}
+
+//vec4 minecraft_sample_lightmap(sampler2D lightMap, ivec2 uv) {
+//    return texture(lightMap, clamp(uv / 256.0, vec2(0.5 / 16.0), vec2(15.5 / 16.0)));
+//}
+
+vec4 sample_lightmap(sampler2D lightMap, ivec2 uv) {
+    return texelFetch(lightMap, (uv & 255) >> 4, 0);
+}
+
+vec4 linear_fog(vec4 inColor, float vertexDistance, float fogStart, float fogEnd, vec4 fogColor) {
+    if (vertexDistance <= fogStart) {
+        return inColor;
+    }
+
+    float fogValue = vertexDistance < fogEnd ? smoothstep(fogStart, fogEnd, vertexDistance) : 1.0;
+    return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a);
+}
