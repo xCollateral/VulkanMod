135781749da21d8d54644e332c6b917f5fb8a1ee
xCollateral <>
2023-12-24 17:13:36 +0100

    Add stencil aspect bit

--- src/main/java/net/vulkanmod/vulkan/texture/VulkanImage.java
+++ src/main/java/net/vulkanmod/vulkan/texture/VulkanImage.java
@@ -136,7 +136,15 @@ public class VulkanImage {
     }
 
     public static int getAspect(int format) {
-        return isDepthFormat(format) ? VK_IMAGE_ASPECT_DEPTH_BIT : VK_IMAGE_ASPECT_COLOR_BIT;
+        return switch (format) {
+            case VK_FORMAT_D24_UNORM_S8_UINT, VK_FORMAT_D32_SFLOAT_S8_UINT
+                    -> VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
+
+            case VK_FORMAT_X8_D24_UNORM_PACK32, VK_FORMAT_D32_SFLOAT,
+                    VK_FORMAT_D16_UNORM -> VK_IMAGE_ASPECT_DEPTH_BIT;
+
+            default -> VK_IMAGE_ASPECT_COLOR_BIT;
+        };
     }
 
     public static boolean isDepthFormat(int format) {
