de3e4d2023fe75fc0fc58f39b70409fbfcac1ef6
xCollateral <>
2024-05-12 18:18:54 +0200

    Replace mixin overwrite to improve compatibility

--- src/main/java/net/vulkanmod/mixin/wayland/InputConstantsM.java
+++ src/main/java/net/vulkanmod/mixin/wayland/InputConstantsM.java
@@ -2,10 +2,10 @@ package net.vulkanmod.mixin.wayland;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.vulkanmod.config.Platform;
-import net.vulkanmod.config.video.VideoModeManager;
 import org.lwjgl.glfw.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(InputConstants.class)
 public class InputConstantsM {
@@ -13,10 +13,9 @@ public class InputConstantsM {
      * @author
      * @reason Setting the cursor position is not supported on Wayland
      */
-    @Overwrite
-    public static void grabOrReleaseMouse(long window, int inputMode, double xpos, double ypos) {
+    @Redirect(method = "grabOrReleaseMouse", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwSetCursorPos(JDD)V"))
+    private static void grabOrReleaseMouse(long window, double xpos, double ypos) {
         if (!Platform.isWayLand())
             GLFW.glfwSetCursorPos(window, xpos, ypos);
-        GLFW.glfwSetInputMode(window, 208897, inputMode);
     }
 }
