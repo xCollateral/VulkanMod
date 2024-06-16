2c566593b8cbf93e8778dc87732aac7c62479728
thr3343 <125277899+thr3343@users.noreply.github.com>
2024-06-16 19:14:19 +0100

    Fix depth buffer artifacts with Glowing PostEffect

--- src/main/java/net/vulkanmod/vulkan/pass/DefaultMainPass.java
+++ src/main/java/net/vulkanmod/vulkan/pass/DefaultMainPass.java
@@ -37,6 +37,7 @@ public class DefaultMainPass implements MainPass {
     private void createRenderPasses() {
         RenderPass.Builder builder = RenderPass.builder(this.mainFramebuffer);
         builder.getColorAttachmentInfo().setFinalLayout(VK_IMAGE_LAYOUT_PRESENT_SRC_KHR);
+        builder.getDepthAttachmentInfo().setOps(VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_STORE);
         this.mainRenderPass = builder.build();
 
         // Create an auxiliary RenderPass needed in case of main target rebinding
