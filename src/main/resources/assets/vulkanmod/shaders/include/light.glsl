69f385a246d01455effebf71223bbd3859c1adeb
xCollateral <103696619+xCollateral@users.noreply.github.com>
2024-06-27 23:44:39 +0200

    Revert lighting computation

--- src/main/resources/assets/vulkanmod/shaders/include/light.glsl
+++ src/main/resources/assets/vulkanmod/shaders/include/light.glsl
@@ -8,12 +8,13 @@ vec4 minecraft_mix_light(vec3 lightDir0, vec3 lightDir1, vec3 normal, vec4 color
     lightDir1 = normalize(lightDir1);
     float light0 = max(0.0, dot(lightDir0, normal));
     float light1 = max(0.0, dot(lightDir1, normal));
-    float lightAccum = fma((light0 + light1), MINECRAFT_LIGHT_POWER, MINECRAFT_AMBIENT_LIGHT);
+    float lightAccum = min(1.0, fma((light0 + light1), MINECRAFT_LIGHT_POWER, MINECRAFT_AMBIENT_LIGHT));
     return vec4(color.rgb * lightAccum, color.a);
 }
 
 vec4 minecraft_sample_lightmap(sampler2D lightMap, ivec2 uv) {
-    return texelFetch(lightMap, bitfieldExtract(uv, 4, 8), 0); //return texture(lightMap, clamp(uv / 256.0, vec2(0.5 / 16.0), vec2(15.5 / 16.0)));
+    return texelFetch(lightMap, bitfieldExtract(uv, 4, 8), 0);
+    //return texture(lightMap, clamp(uv / 256.0, vec2(0.5 / 16.0), vec2(15.5 / 16.0)));
 }
 
 vec4 sample_lightmap(sampler2D lightMap, ivec2 uv) {
