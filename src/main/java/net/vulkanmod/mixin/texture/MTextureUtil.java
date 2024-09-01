c95e2bd81d00b7bf1f87f1bbeef579c5fd1dbdd7
seji1 <152516748+seji1@users.noreply.github.com>
2024-09-01 17:25:18 +0200

    Refactor GuiRenderer class
    - Remove GuiBatchRenderer
    - Add batching support to GuiRenderer

--- src/main/java/net/vulkanmod/mixin/texture/MTextureUtil.java
+++ src/main/java/net/vulkanmod/mixin/texture/MTextureUtil.java
@@ -21,27 +21,27 @@ public class MTextureUtil {
         return GlTexture.genTextureId();
     }
 
-    /**
-     * @author
-     */
-    @Overwrite(remap = false)
-    public static void prepareImage(NativeImage.InternalGlFormat internalGlFormat, int id, int mipLevels, int width, int height) {
-        RenderSystem.assertOnRenderThreadOrInit();
-        GlTexture.bindTexture(id);
-        GlTexture glTexture = GlTexture.getBoundTexture();
-        VulkanImage image = glTexture.getVulkanImage();
-
-        if(image == null || image.mipLevels != mipLevels || image.width != width || image.height != height) {
-            if(image != null)
-                image.free();
-
-            image = new VulkanImage.Builder(width, height)
-                    .setLinearFiltering(false)
-                    .setClamp(false)
-                    .createVulkanImage();
-
-            glTexture.setVulkanImage(image);
-            VTextureSelector.bindTexture(image);
-        }
-    }
+//    /**
+//     * @author
+//     */
+//    @Overwrite(remap = false)
+//    public static void prepareImage(NativeImage.InternalGlFormat internalGlFormat, int id, int mipLevels, int width, int height) {
+//        RenderSystem.assertOnRenderThreadOrInit();
+//        GlTexture.bindTexture(id);
+//        GlTexture glTexture = GlTexture.getBoundTexture();
+//        VulkanImage image = glTexture.getVulkanImage();
+//
+//        if(image == null || image.mipLevels != mipLevels || image.width != width || image.height != height) {
+//            if(image != null)
+//                image.free();
+//
+//            image = new VulkanImage.Builder(width, height)
+//                    .setLinearFiltering(false)
+//                    .setClamp(false)
+//                    .createVulkanImage();
+//
+//            glTexture.setVulkanImage(image);
+//            VTextureSelector.bindTexture(image);
+//        }
+//    }
 }
