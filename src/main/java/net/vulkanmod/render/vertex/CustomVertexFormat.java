2545ce366980408d7d7cb8c73333f0609945103a
thr3343 <125277899+thr3343@users.noreply.github.com>
2024-09-02 23:00:24 +0100

    Optimize format based on device architecture
    Select 2 Byte alignment by default (including Nvidia), otherwise use 4 Bytes on AMD (GCN)

--- src/main/java/net/vulkanmod/render/vertex/CustomVertexFormat.java
+++ src/main/java/net/vulkanmod/render/vertex/CustomVertexFormat.java
@@ -3,12 +3,15 @@ package net.vulkanmod.render.vertex;
 import com.google.common.collect.ImmutableMap;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormatElement;
+import net.vulkanmod.vulkan.Vulkan;
+import net.vulkanmod.vulkan.device.Device;
+import net.vulkanmod.vulkan.device.DeviceManager;
 
 public class CustomVertexFormat {
 
     public static final VertexFormatElement ELEMENT_POSITION = new VertexFormatElement(0,VertexFormatElement.Type.SHORT, VertexFormatElement.Usage.POSITION, 4);
     public static final VertexFormatElement ELEMENT_COLOR = new VertexFormatElement(0, VertexFormatElement.Type.UBYTE, VertexFormatElement.Usage.COLOR, 4);
-    public static final VertexFormatElement ELEMENT_UV0 = new VertexFormatElement(0, VertexFormatElement.Type.UINT, VertexFormatElement.Usage.UV, 1);
+    public static final VertexFormatElement ELEMENT_UV0 = Vulkan.getDevice().isAMD() ? new VertexFormatElement(0,VertexFormatElement.Type.UINT, VertexFormatElement.Usage.UV, 1) : new VertexFormatElement(0, VertexFormatElement.Type.USHORT, VertexFormatElement.Usage.UV, 2);
     public static final VertexFormatElement ELEMENT_UV2 = new VertexFormatElement(2, VertexFormatElement.Type.SHORT, VertexFormatElement.Usage.UV, 2);
 
     public static final VertexFormat COMPRESSED_TERRAIN = new VertexFormat(new ImmutableMap.Builder<String, VertexFormatElement>()
